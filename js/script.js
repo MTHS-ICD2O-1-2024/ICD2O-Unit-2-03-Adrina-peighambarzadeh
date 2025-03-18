b9fbbfce2e97cb2a737a25f4c0f7236f6083d13d
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-03-18 16:38:46 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-// Copyright (c) 2025 Adrina. peighambarzadeh All rights reserved
-//
-// Created by: Adrina. peighambarzadeh
-// Created on: mar 2025
-// This file contains the JS functions for index.html
-
 /**
- * Do basic math.
+ * Function to handle the click event.
  */
 function enterClicked() {
-  // input
-  const streetNumber = document.getElementById("street-number").value
-  const streetName = parseInt(document.getElementById("street-name").value)
-  // output
-  document.getElementById("user-info").innerHTML =
-    "Your addres is: " + streetNumber + ", " + streetName + "."
+  // Get input values
+  const streetNumber = document.getElementById("street-number").value;
+  const streetName = document.getElementById("street-name").value;
+  // Output the address with "Address is: " before it
+  document.getElementById("user-info").innerHTML = ` Your Address is: ${streetNumber}, ${streetName}.`;
 }
